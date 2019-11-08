28f2e81da8da353a25bb5b10331dbba9f843496e
Jose Alonso <jalonsoriveiro@danielcastelao.org>
2019-11-08 14:19:32 +0100

    Salida Saludo

--- src/com/HolaQueTal/Main.java
+++ src/com/HolaQueTal/Main.java
@@ -6,6 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hola Mundo");
+        System.out.println("Chau");
+    //TODO
+        // comprobar que el numero es negativo
     }
 }
